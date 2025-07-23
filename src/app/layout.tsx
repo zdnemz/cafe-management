2af589f13541c54fcf242c72c3752a4e28bbe526
zdnemz <zidanemz69@gmail.com>
2025-07-24 05:08:54 +0800

    feat(app): configurate next-theme and add root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Roboto } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
+import { ThemeProvider } from "@/providers/theme-provider";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const roboto = Roboto({
+  variable: "--font-roboto-sans",
   subsets: ["latin"],
 });
 
@@ -23,11 +20,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+    <html lang="en" suppressHydrationWarning>
+      <body className={cn(roboto.variable, "antialiased")}>
+        {" "}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
---
+++ src/components/layouts/RootLayout.tsx
@@ -0,0 +1,33 @@
+import { cn } from "@/lib/utils";
+import * as React from "react";
+
+export interface RootLayoutProps {
+  className?: string;
+  children: React.ReactNode | React.ReactNode[];
+  header?: React.ReactNode;
+  footer?: React.ReactNode;
+}
+
+const DEFAULT_PADDING =
+  "*:px-6 *:sm:px-12 *:md:px-24 *:lg:px-36 *:xl:px-48 *:2xl:px-60";
+
+export default function RootLayout({
+  className,
+  children,
+  header,
+  footer,
+}: RootLayoutProps) {
+  return (
+    <>
+      {header && (
+        <header className={cn("fixed z-50 w-full", DEFAULT_PADDING)}>
+          {header}
+        </header>
+      )}
+      <main className={cn("relative *:py-12", className, DEFAULT_PADDING)}>
+        {children}
+      </main>
+      {footer && <footer className={cn(DEFAULT_PADDING)}>{footer}</footer>}
+    </>
+  );
+}
